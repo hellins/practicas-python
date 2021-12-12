310b8ee9124421774c866e9a38b02e1f9f390798
hellins <hellin.solorzano1@gmail.com>
2021-12-12 14:28:52 -0400

    dos

--- CursoDePython/practica2.py
+++ CursoDePython/practica2.py
@@ -1,1 +1,9 @@
+'''
+Practica secuencial 2
+script en Python que salude al usuario de forma personañozada. El nombre del usuario estara \
+    almacenado en uan variable.
+'''
 
+usuario = "Jose Perez"
+#print("Hola", usuario, "como esta tu familia.")
+print(f"Hola {usuario} mucho gusto en conocerte.")
