644cb101977b60972b32417bf1799c844480afdc
hellins <hellin.solorzano1@gmail.com>
2021-12-12 15:31:10 -0400

    dos

--- CursoDePython/practica2.py
+++ CursoDePython/practica2.py
@@ -1,9 +1,24 @@
 '''
-Practica secuencial 2
+Practica secuencial 2 y 3
 script en Python que salude al usuario de forma personañozada. El nombre del usuario estara \
     almacenado en uan variable.
-'''
 
-usuario = "Jose Perez"
+#usuario = "Jose Perez"
 #print("Hola", usuario, "como esta tu familia.")
+#print(f"Hola {usuario} mucho gusto en conocerte.")
+usuario = input("Como te llamas: ")
 print(f"Hola {usuario} mucho gusto en conocerte.")
+
+valor_1 = int(input("ingresa un numero: "))
+valor_2 = int(input("ingrasa el segundo numero: "))
+
+suma = valor_1 + valor_2
+
+print(f"{valor_1} + {valor_2} = {suma}")
+'''
+# Calacular el area de un triangulo
+print("Programa para calcular el area de una triangulo")
+base = float(input("Ingresa la distacia de la base: "))
+altura = float(input("insgrsa la distacia de la altuta: "))
+resultado = base * altura / 2
+print(f"La Base {base} + La Altura {altura} / 2 = el area es: {resultado}")
