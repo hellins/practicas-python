291437e9d196c7c3750ecbfca87449e4c7262cb6
hellins <hellin.solorzano1@gmail.com>
2021-12-12 17:17:24 -0400

    dos

---
+++ CursoDePython/practica3.py
@@ -0,0 +1,10 @@
+'''
+Selectiva simple
+script en paython 
+'''
+edad = int(input("hola cuantos años tienes: "))
+if edad >= 18:
+    print("Eres Mayor de Edad")
+else:
+    print("Eres Menor de Edad")
+print("Que tengas un excelente dia")   
