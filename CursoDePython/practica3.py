469c1a653ebba7f0c3cb37afbd924942b296273e
hellins <hellin.solorzano1@gmail.com>
2021-12-12 17:34:16 -0400

    dos

--- CursoDePython/practica3.py
+++ CursoDePython/practica3.py
@@ -1,10 +1,23 @@
 '''
 Selectiva simple
 script en paython 
-'''
+
 edad = int(input("hola cuantos años tienes: "))
 if edad >= 18:
     print("Eres Mayor de Edad")
 else:
     print("Eres Menor de Edad")
 print("Que tengas un excelente dia")   
+'''
+import random
+
+secreto = random.randint(1, 10)
+
+print("he generado un numero aleatorio del 1 al 10. Intenta adivonarlo")
+numero = int(input("Cual cres que sea el numero? "))
+
+if numero == secreto:
+    print(f"Lograste Aserta el numero: {secreto}")
+    
+print("Sigue disfrutando tu dia")
+    
