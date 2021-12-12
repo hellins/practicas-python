77048907400d7abfac1ba71932e8653aaff1c0af
hellins <hellin.solorzano1@gmail.com>
2021-12-12 14:14:45 -0400

    segundo

--- CursoDePython/practica1.py
+++ CursoDePython/practica1.py
@@ -1,2 +1,20 @@
-print("Hola python")
-[1, 2, 3]
+"""
+Juego para adivinar el numero qu estas pensando
+"""
+
+print("Vamos a jugar")
+print("Te voy a solicitar que realices una operacion y posterior mente adivinare \
+    el numero que tienes en mente")
+input("Presiona enter cuando estes List@")
+print("Piensa en un numero")
+input("Presiona enter cuando estes List@")
+print("Multiplica ese numero por 2")
+input("Presiona enter cuando estes List@")
+print("Sumale 10 a ese valor")
+input("Presiona enter cuando estes List@")
+print("dividelo entre 2 ")
+input("Presiona enter cuando estes List@")
+print("Restale tu numero inicial")
+input("Presiona enter cuando estes List@")
+input("El numero que tienes en mente es .....")
+input("5 alv")
