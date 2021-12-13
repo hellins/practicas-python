800b03da1b36d884ec47baa794375f423bd00e7a
hellins <hellin.solorzano1@gmail.com>
2021-12-13 12:26:33 -0400

    dos

--- CursoDePython/practica4.py
+++ CursoDePython/practica4.py
@@ -19,7 +19,7 @@ print("Sigue disfrutando tu Dia")
 Script de python que pregunte al usuario cuanto es la temperatura del termometroy si ese valor
 se encuentra entre 18 y 27 que indique la temperatura es agradable
 '''
-print("Se medira la temperatura")
+'''print("Se medira la temperatura")
 
 temp = int(input("introduzca el valor de la temperatura: "))
 
@@ -27,3 +27,32 @@ if 18<= temp <=27:
     print("La temperatura es agradable")
 print("Nos vemos pronto")  
 print("hola")  
+'''
+# Sistema de redondeo de notas
+# El usuario debe cargar la calificacion, si la calificacion le falta 0,4 o menos para
+# el siguiente multiplo de 10 , entonces su calificacion sera redondeada al siguiente 
+# multiplo de 10, en caso contrario su calificacion no sera modidicada.
+
+import math
+
+nota = float(input("Ingresa tu Calificacion: "))
+
+calificacion = round(nota)
+
+print(f"Tu calificacion es: {calificacion}")
+if calificacion >= 10:
+    print("Bien hecho aprobaste la materia !!!")
+    if calificacion >= 16:
+        print("Excelente eximiste la Materia")
+        
+print("Sigue estudiando")
+        
+'''
+numero = 1.5
+redondeado = round(numero)
+redondeado_abajo = math.floor(numero)
+redondeado_arriba = math.ceil(numero)
+print("Redondeado con round: {}".format(redondeado))
+print("Redondeado con floor (hacia abajo): {}".format(redondeado_abajo))
+print("Redondeado con ceil (hacia arriba): {}".format(redondeado_arriba))
+'''
