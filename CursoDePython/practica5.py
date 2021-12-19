7b50c8e7f90ad69149b8027b9dd701c111ad31a5
hellins <hellin.solorzano1@gmail.com>
2021-12-19 16:22:47 -0400

    dos

--- CursoDePython/practica5.py
+++ CursoDePython/practica5.py
@@ -22,8 +22,24 @@ else:
 # Estructura Selectiva Multiple
 '''
 Script en Python que solicite al usuario una calificacion y una cantidad de asistencias a un
-curso. Si la calificacion y la cantidad de asistencias son aprobatorias en tonces se le 
+curso. Si la calificacion y la cantidad de asistencias son aprobatorias entonces se le 
 felicita por su logro; en caso contrario sele indicara en que fallo. La cailificacion
 minima aprobatoria sera  de 60 y la cantidad minima de asiatencia sera de 24.
 '''
 
+MIN_CAL = 60
+MIN_ASI = 24
+
+print("Por favon inrgresa los siguientes datos: ")
+cal = int(input("Colocca tu calificacion: "))
+asi = int(input("Ingrrsa tu Asistencia: "))
+          
+if cal >= MIN_CAL and asi >= MIN_ASI:
+    print("Aprobaste la Materia Felicitaciones")
+elif cal < MIN_CAL and asi >= MIN_ASI:
+    print(f"Calificacion insuficiente. El minimo es: {MIN_CAL}")
+elif cal >= MIN_CAL and asi < MIN_ASI:
+    print(f"Asisstencias insuficientes. La minima es: {MIN_ASI}")
+else:
+    print(f"Calificacion y Asistencias insuficiente. La Calificacion mininima es: {MIN_CAL} y la asistencia minima es {MIN_ASI}")
+print("Que tengas un buen dia")  
