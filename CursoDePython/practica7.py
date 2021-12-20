fb5254e86c91a5d3f56dc55d9c2040d3d51052f7
hellins <hellin.solorzano1@gmail.com>
2021-12-20 13:12:53 -0400

    dos

---
+++ CursoDePython/practica7.py
@@ -0,0 +1,13 @@
+'''
+Selectiva Multuiple
+Script en Python que simule una calculadora con la operaciones arismeticas basicas. El menu mostrado sera el siguiente:
+
+1) Suma
+2) Resta
+3) Multiplicacion
+4) Division
+5) Division Entera
+6) Modulo
+7) Ptencia
+'''
+
