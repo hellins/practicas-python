c9897aefef68f5893408461d87ae0459096d21d2
hellins <hellin.solorzano1@gmail.com>
2021-12-20 18:30:24 -0400

    dos

--- CursoDePython/practica7.py
+++ CursoDePython/practica7.py
@@ -8,6 +8,53 @@ Script en Python que simule una calculadora con la operaciones arismeticas basic
 4) Division
 5) Division Entera
 6) Modulo
-7) Ptencia
+7) Potencia
 '''
 
+print('''                                   CALCULADORA
+1) Suma
+2) Resta
+3) Multiplicacion
+4) Division
+5) Division Entera
+6) Modulo
+7) Potencia
+      ''')
+
+SUMA = 1
+RESTA = 2
+MULTI = 3
+DIVI = 4
+DIVI_ENTE = 5
+MODULO = 6
+POTENCIA = 7
+
+opc = int(input("Seleccione Operacion Arismetica: "))
+if SUMA <= opc <= POTENCIA:
+    if opc == SUMA:
+        suma1 = int(input("Ingresa primer numero a sumar: "))
+        suma2 = int(input("Ingresa segundo numero a sumar: "))
+        resuma = suma1 + suma2
+        print(f"El resultado de la suma de {suma1} + {suma2} es: {resuma}")
+    elif opc == RESTA:
+        resta1 = int(input("ingresa primer numero a restar: "))
+        resta2 = int(input("Ingresa segundo numero a restar: "))
+        resuresta = resta1 - resta2
+        print(f"El resultado de la resta de {resta1} - {resta2} es: {resuresta}")
+    elif opc == MULTI:
+        multi1 = int(input("Ingresa primer numero a Multiplicar: "))
+        multi2 = int(input("Ingresa segundo numero a Multiplicar: "))        
+        resumulti = multi1 * multi2
+        print(f"El Resultado de la Multiplicaion de {multi1} x {multi2} es: {resumulti}")
+    elif opc == DIVI:
+        divi1 = int(input("ingresa el dividendo: "))
+        divi2 = int(input(" ingrese el divisor: "))
+        if divi2 == 0:
+            print("No se puede dividir entre cero (0)")
+        else:
+            resudivi = divi1 / divi2
+            print(f"El resultado de la Division de {divi1} / {divi2} es: {resudivi}")
+    #elif opc == DIVI_ENTE  
+
+else:
+    print("Opcion no valida")
