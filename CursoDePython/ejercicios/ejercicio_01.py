4c04982a26957bf54d96561ed9e0d011b38e3951
hellins <hellin.solorzano1@gmail.com>
2021-12-28 16:51:14 -0400

    e01

--- CursoDePython/ejercicios/ejercicio_01.py
+++ CursoDePython/ejercicios/ejercicio_01.py
@@ -6,4 +6,36 @@ script en Python que implemente el juego de adivinar un numero, pero esta vez en
 import os
 import random
 
-secreto = random.
+inferior = 1
+superoir = 100
+secreto = random.randint(1, 100)
+usuario = -1
+maquina = -1
+
+while usuario != secreto and maquina != secreto:
+    os.system("clear")
+    print("Maquina ¿Cual crees que sea el numero sercreto? ")
+    maquina = random.randint(inferior, superoir)
+    print(f"Maquina: {maquina}")
+    if maquina < secreto:
+        print("Tu numero es menor")
+        inferior = maquina + 1
+    elif maquina > secreto:
+        print("Tu numero es mayor")
+        superoir = maquina - 1
+    else:
+        print("Ha ganado la Maquina ....")
+    if maquina != secreto:
+        usuario = int(input("Usuario, ¿Cual crees que sea el número secreto: ?"))
+        if usuario < secreto:
+            print("tu numero es menor")
+            if usuario > inferior:
+                inferior = usuario +1
+        elif usuario > secreto:
+            print("Tu numero es mayor")
+            if usuario < superoir:
+                superoir = usuario - 1
+        else:
+            print("El usuario  ha  ganado")
+    input("presina enter para continuar.....")
+input("Nos vemos.....")
